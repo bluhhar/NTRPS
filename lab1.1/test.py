aed72a741e41542c11bc2825733bdab447bb2bea
bluhhar <bluhhar@gmail.com>
2023-09-16 01:01:32 +0300

    +main in 1 variant

--- lab1.1/test.py
+++ lab1.1/test.py
@@ -2,22 +2,27 @@ import requests
 import csv
 from datetime import datetime, timedelta
 
-start_date = datetime(2023, 8, 30)
+def main():
+    start_date = datetime(2023, 9, 14)
 
-end_date = datetime.now()
+    end_date = datetime.now()
 
-with open('dataset.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Date', 'USD'])
+    with open('dataset.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Date', 'USD'])
 
-    current_date = start_date
-    while current_date <= end_date:
-        url = f'https://www.cbr-xml-daily.ru/archive/{current_date.year}/{current_date.strftime("%m")}/{current_date.strftime("%d")}/daily_json.js'
-        
-        response = requests.get(url, headers={"User-Agent":"Mozilla/5.0"})
-        if response.status_code == 200:
-            data = response.json()
-            today = current_date.strftime('%Y-%m-%d')
-            writer.writerow([today, data['Valute']['USD']['Value']])
-        
-        current_date += timedelta(days=1)
+        current_date = start_date
+        while current_date <= end_date:
+            url = f'https://www.cbr-xml-daily.ru/archive/{current_date.year}/{current_date.strftime("%m")}/{current_date.strftime("%d")}/daily_json.js'
+            
+            response = requests.get(url, headers={"User-Agent":"Mozilla/5.0"})
+            if response.status_code == 200:
+                data = response.json()
+                today = current_date.strftime('%Y-%m-%d')
+                writer.writerow([today, data['Valute']['USD']['Value']])
+            
+            current_date += timedelta(days=1)
+
+
+if __name__ == "__main__":
+    main()
