2bcfc7c5c16d1b4209eb70e0ec5431ef6a077039
bluhhar <bluhhar@gmail.com>
2023-09-16 11:56:36 +0300

    +some little changes v1 user agent

--- lab1.1/test.py
+++ lab1.1/test.py
@@ -2,7 +2,7 @@ import requests
 import csv
 from datetime import datetime, timedelta
 
-def writedataset(start_date = datetime(2023, 9, 14), end_date = datetime.now()):
+def writedataset(start_date = datetime(2023, 9, 10), end_date = datetime.now()):
     with open('dataset.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Date', 'USD'])
@@ -11,7 +11,7 @@ def writedataset(start_date = datetime(2023, 9, 14), end_date = datetime.now()):
         while current_date <= end_date:
             url = f'https://www.cbr-xml-daily.ru/archive/{current_date.year}/{current_date.strftime("%m")}/{current_date.strftime("%d")}/daily_json.js'
             
-            response = requests.get(url, headers={'User-Agent":"Mozilla/5.0'})
+            response = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
             if response.status_code == 200:
                 data = response.json()
                 today = current_date.strftime('%Y-%m-%d')
@@ -29,9 +29,7 @@ def readdataset():
 
 def main():
     writedataset()
-
     readdataset()
-
 
 if __name__ == "__main__":
     main()
