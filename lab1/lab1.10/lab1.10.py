17c6011735c6f402a3cb9b07a6d0c1823fd08efc
bluhhar <bluhhar@gmail.com>
2023-09-19 21:07:13 +0300

    +title and link for 10 variant

--- lab1/lab1.10/lab1.10.py
+++ lab1/lab1.10/lab1.10.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import re
 
 from bs4 import BeautifulSoup
 
@@ -19,18 +20,33 @@ def check_repo_dataset(class_name):
     else:
         return class_folder
 
+def parse_rating(str):
+    str = re.search(r'\d+', str)
+    if str:
+        number = str.group()
+        return number
+    else:
+        return 0
+
 def download_reviews(num_reviews, star_rating):
     pages = 2
     search_url = f'https://www.livelib.ru/reviews/~{pages}#reviews'
     response = requests.get(search_url, headers={'User-Agent':'Mozilla/5.0'})
     soup = BeautifulSoup(response.text, 'html.parser')
     for review in soup.find_all('article', class_='review-card lenta__item'):
-        rating_span = review.find('span', class_='lenta-card__mymark')
-        if rating_span:
-            rating = rating_span.text
-            print("rating", rating)
+        rating_tag = review.find('span', class_='lenta-card__mymark')
+        title_tag = review.find('h3', class_='lenta-card__title')
+        link = title_tag.find('a')['href']
+        title = title_tag.find('a').text
+        if rating_tag:
+            rating = rating_tag.text
+            rating = parse_rating(rating)
+            
+            print('rating', rating)
+            print('title', title)
+            print('link', link)
         else: #нехорошие люди не ставят рейтинг книги
-            print("rating not found")
+            print('rating not found')
 
          
 
