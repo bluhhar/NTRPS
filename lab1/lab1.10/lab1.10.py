18f5e88cd0728ab4cf967e94075b2e7de1996850
bluhhar <bluhhar@gmail.com>
2023-09-19 20:49:04 +0300

    +rating for 10 variant

--- lab1/lab1.10/lab1.10.py
+++ lab1/lab1.10/lab1.10.py
@@ -19,8 +19,24 @@ def check_repo_dataset(class_name):
     else:
         return class_folder
 
+def download_reviews(num_reviews, star_rating):
+    pages = 2
+    search_url = f'https://www.livelib.ru/reviews/~{pages}#reviews'
+    response = requests.get(search_url, headers={'User-Agent':'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for review in soup.find_all('article', class_='review-card lenta__item'):
+        rating_span = review.find('span', class_='lenta-card__mymark')
+        if rating_span:
+            rating = rating_span.text
+            print("rating", rating)
+        else: #нехорошие люди не ставят рейтинг книги
+            print("rating not found")
+
+         
+
 def main():
     check_dataset()
+    download_reviews(2, 1)
 
 if __name__ == '__main__':
     main()
